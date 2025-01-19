26765021ab3a4172d42f8c7adc5c92da32c2dac2
Erfan3738 <138443555+Erfan3738@users.noreply.github.com>
2025-01-19 04:28:33 +0330

    Update CaCo.py

--- model/CaCo.py
+++ model/CaCo.py
@@ -17,11 +17,11 @@ class CaCo(nn.Module):
         self.m = m
 
         self.encoder_q = base_encoder(num_classes=dim)
-        self.encoder_q.conv1 = nn.Conv2d(3, 64, 3, 1, 1, bias=False)
-        self.encoder_q.maxpool = nn.Identity()
+        #self.encoder_q.conv1 = nn.Conv2d(3, 64, 3, 1, 1, bias=False)
+        #self.encoder_q.maxpool = nn.Identity()
         self.encoder_k = base_encoder(num_classes=dim)
-        self.encoder_k.conv1 = nn.Conv2d(3, 64, 3, 1, 1, bias=False)
-        self.encoder_k.maxpool = nn.Identity()
+        #self.encoder_k.conv1 = nn.Conv2d(3, 64, 3, 1, 1, bias=False)
+        #self.encoder_k.maxpool = nn.Identity()
         dim_mlp = self.encoder_q.fc.weight.shape[1]
         
         self.encoder_q.fc = self._build_mlp(2,dim_mlp,args.mlp_dim,dim,last_bn=False)
