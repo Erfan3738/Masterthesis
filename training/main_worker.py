f284c4300659cea8044c10ab8c15c0e11921c132
Erfan3738 <138443555+Erfan3738@users.noreply.github.com>
2025-01-21 01:24:38 +0330

    Update main_worker.py

--- training/main_worker.py
+++ training/main_worker.py
@@ -81,18 +81,18 @@ def main_worker(gpu, ngpus_per_node, args):
     model = CaCo(models.__dict__[args.arch], args,
                            args.moco_dim, args.moco_m)
 
-    #optimizer = torch.optim.SGD(model.parameters(), init_lr,
+    optimizer = torch.optim.SGD(model.parameters(), init_lr,
                                 #momentum=args.momentum,
                                 #weight_decay=args.weight_decay)
-   # from ops.LARS import SGD_LARC
-    #optimizer = SGD_LARC(optimizer, trust_coefficient=0.001, clip=False, eps=1e-8)
+    from ops.LARS import SGD_LARC
+    optimizer = SGD_LARC(optimizer, trust_coefficient=0.001, clip=False, eps=1e-8)
     #from model.optimizer import  LARS
     #optimizer = LARS(model.parameters(), init_lr,
                          #weight_decay=args.weight_decay,
                          #momentum=args.momentum)
-    from model.optimizer import  AdamW
+    #from model.optimizer import  AdamW
 
-    optimizer = AdamW(model.parameters(),args.lr,betas=(0.9, 0.999),eps=1e-8,weight_decay=args.weight_decay)
+    #optimizer = AdamW(model.parameters(),args.lr,betas=(0.9, 0.999),eps=1e-8,weight_decay=args.weight_decay)
     model.cuda()
     Memory_Bank.cuda()
 
