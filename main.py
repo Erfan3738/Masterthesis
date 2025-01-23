1cad92c724f1e97dc34f79392369ca45c7e35668
Erfan3738 <138443555+Erfan3738@users.noreply.github.com>
2025-01-24 02:34:05 +0330

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def main(args):
         mp.spawn(main_worker, nprocs=ngpus_per_node, args=(ngpus_per_node, args))
     else:
         # Simply call main_worker function
-        main_worker(args.gpu, ngpus_per_node, args)
+        main_worker(args)
 if __name__ == '__main__':
     #use_cuda = torch.cuda.is_available()
     #print("starting check cuda status",use_cuda)
