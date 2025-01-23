369e0825481e001f30e1b8a25fb57b8e75448242
Erfan3738 <138443555+Erfan3738@users.noreply.github.com>
2025-01-23 23:45:17 +0330

    Update CaCo.py

--- model/CaCo.py
+++ model/CaCo.py
@@ -16,11 +16,15 @@ class CaCo(nn.Module):
         # create the encoders
         # num_classes is the output fc dimension
         self.encoder_q = base_encoder(num_classes=dim)
+        self.encoder_q.conv1 = nn.Conv2d(3, 64, 3, 1, 1, bias=False)
+        self.encoder_q.maxpool = nn.Identity()
         self.encoder_k = base_encoder(num_classes=dim)
+        self.encoder_k.conv1 = nn.Conv2d(3, 64, 3, 1, 1, bias=False)
+        self.encoder_k.maxpool = nn.Identity()
         dim_mlp = self.encoder_q.fc.weight.shape[1]
         # we do not keep 
-        self.encoder_q.fc = self._build_mlp(3,dim_mlp,args.mlp_dim,dim,last_bn=False)
-        self.encoder_k.fc = self._build_mlp(3, dim_mlp, args.mlp_dim, dim,last_bn=False)
+        self.encoder_q.fc = self._build_mlp(2,dim_mlp,args.mlp_dim,dim,last_bn=False)
+        self.encoder_k.fc = self._build_mlp(2, dim_mlp, args.mlp_dim, dim,last_bn=False)
         
         #self.encoder_q.fc = self._build_mlp(2,dim_mlp,args.mlp_dim,dim,last_bn=True)
         #self.encoder_k.fc = self._build_mlp(2, dim_mlp, args.mlp_dim, dim, last_bn=True)
