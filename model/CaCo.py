f4b141ef0658ee5166e7e25406752ee0f4e10be0
Erfan3738 <138443555+Erfan3738@users.noreply.github.com>
2025-01-25 13:38:41 +0330

    Update CaCo.py

--- model/CaCo.py
+++ model/CaCo.py
@@ -101,13 +101,13 @@ class CaCo(nn.Module):
             q = self.encoder_k(im_q_, use_feature=False)  # keys: NxC
             q = nn.functional.normalize(q, dim=1)
             q = self._batch_unshuffle_single_gpu(q, idx_unshuffle)
-            q = q.detach()
+            
 
             im_k_, idx_unshuffle1 = self._batch_shuffle_single_gpu(im_k)
             k = self.encoder_k(im_k_, use_feature=False)  # keys: NxC
             k = nn.functional.normalize(k, dim=1)
             k = self._batch_unshuffle_single_gpu(k, idx_unshuffle1)
-            k = k.detach()
+            
 
         return q_pred, k_pred, q, k
     def forward_withoutpred_multicrop(self,im_q_list,im_k,moco_momentum):
