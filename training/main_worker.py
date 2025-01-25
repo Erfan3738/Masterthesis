fbdce260d0a54440ba04d94c22e4606275eec98a
Erfan3738 <138443555+Erfan3738@users.noreply.github.com>
2025-01-25 11:52:37 +0330

    Update main_worker.py

--- training/main_worker.py
+++ training/main_worker.py
@@ -67,7 +67,7 @@ def main_worker(args):
 
     Memory_Bank = CaCo_PN(args.cluster,args.moco_dim)
 
-    model = CaCo( args,
+    model = CaCo(models.__dict__[args.arch], args,
                            args.moco_dim, args.moco_m)
 
     
