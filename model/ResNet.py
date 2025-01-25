25e9c6e7a5e6d71c88ab6b60d2596f4fe1cb6163
Erfan3738 <138443555+Erfan3738@users.noreply.github.com>
2025-01-25 09:32:03 +0330

    Update ResNet.py

--- model/ResNet.py
+++ model/ResNet.py
@@ -54,7 +54,7 @@ class BasicBlock(nn.Module):
     ) -> None:
         super(BasicBlock, self).__init__()
         if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
+            norm_layer = nn.LayerNorm
         if groups != 1 or base_width != 64:
             raise ValueError('BasicBlock only supports groups=1 and base_width=64')
         if dilation > 1:
@@ -280,7 +280,7 @@ class ResNet(nn.Module):
     ) -> None:
         super(ResNet, self).__init__()
         if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
+            norm_layer = nn.LayerNorm
         self._norm_layer = norm_layer
 
         self.inplanes = 64
