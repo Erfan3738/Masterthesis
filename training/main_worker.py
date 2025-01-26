90bf852e366d27d5abe5979f4c2e25f54acef152
Erfan3738 <138443555+Erfan3738@users.noreply.github.com>
2025-01-26 07:01:08 +0330

    Update main_worker.py

--- training/main_worker.py
+++ training/main_worker.py
@@ -78,10 +78,10 @@ def main_worker(args):
  
     from model.optimizer import  AdamW
     from model.optimizer import  LARS
-    #optimizer = AdamW(model.parameters())
-    optimizer = LARS(model.parameters(), init_lr,
-                         weight_decay=args.weight_decay,
-                         momentum=args.momentum)
+    optimizer = AdamW(model.parameters())
+    #optimizer = LARS(model.parameters(), init_lr,
+                         #weight_decay=args.weight_decay,
+                         #momentum=args.momentum)
     model.cuda()
     Memory_Bank.cuda()
     print("per gpu batch size: ",args.batch_size)
