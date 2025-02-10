df67d89cdcc4fe34f12ed5b4159887beba83b687
Fateme <87362524+ftmsadeghi@users.noreply.github.com>
2025-02-10 14:56:54 +0330

    Update ResNet.py

--- model/ResNet.py
+++ model/ResNet.py
@@ -638,3 +638,166 @@ def wide_resnet101_2(pretrained: bool = False, progress: bool = True, **kwargs: 
     kwargs['width_per_group'] = 64 * 2
     return _resnet('wide_resnet101_2', Bottleneck, [3, 4, 23, 3],
                    pretrained, progress, **kwargs)
+#------------------------------------------------------------------------------------------------
+class Bottleneck1(nn.Module):
+    outchannel_ratio = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(Bottleneck1, self).__init__()
+        self.bn1 = nn.BatchNorm2d(inplanes)
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, (planes*1), kernel_size=3, stride=stride,
+                               padding=1, bias=False)
+        self.bn3 = nn.BatchNorm2d((planes*1))
+        self.conv3 = nn.Conv2d((planes*1), planes * Bottleneck.outchannel_ratio, kernel_size=1, bias=False)
+        self.bn4 = nn.BatchNorm2d(planes * Bottleneck.outchannel_ratio)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+
+        out = self.bn1(x)
+        out = self.conv1(out)
+
+        out = self.bn2(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+
+        out = self.bn3(out)
+        out = self.relu(out)
+        out = self.conv3(out)
+
+        out = self.bn4(out)
+
+        if self.downsample is not None:
+            shortcut = self.downsample(x)
+            featuremap_size = shortcut.size()[2:4]
+        else:
+            shortcut = x
+            featuremap_size = out.size()[2:4]
+
+        batch_size = out.size()[0]
+        residual_channel = out.size()[1]
+        shortcut_channel = shortcut.size()[1]
+
+        if residual_channel != shortcut_channel:
+            padding = torch.autograd.Variable(torch.cuda.FloatTensor(batch_size, residual_channel - shortcut_channel, featuremap_size[0], featuremap_size[1]).fill_(0))
+            out += torch.cat((shortcut, padding), 1)
+        else:
+            out += shortcut
+
+        return out
+
+class PyramidNet(nn.Module):
+
+    def __init__(
+            self,
+            block: Type[Union[BasicBlock1, Bottleneck1]],
+            
+            num_classes: int = 10,
+            zero_init_residual: bool = False,
+            groups: int = 1,
+            width_per_group: int = 64,
+            replace_stride_with_dilation: Optional[List[bool]] = None,
+            norm_layer: Optional[Callable[..., nn.Module]] = None
+    ) -> None:
+        super(PyramidNet, self).__init__()
+
+        self.alpha = 300
+        self.depth = 32
+        self.inplanes = 16
+        
+        if block == Bottleneck1:
+            n = int((self.depth - 2) / 9)
+            
+        else:
+            n = int((self.depth - 2) / 6)
+            
+
+        self.addrate = self.alpha / (3*n*1.0)
+
+        self.input_featuremap_dim = self.inplanes
+        self.conv1 = nn.Conv2d(3, self.input_featuremap_dim, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.input_featuremap_dim)
+
+        self.featuremap_dim = self.input_featuremap_dim
+        self.layer1 = self.pyramidal_make_layer(block, n)
+        self.layer2 = self.pyramidal_make_layer(block, n, stride=2)
+        self.layer3 = self.pyramidal_make_layer(block, n, stride=2)
+
+        self.final_featuremap_dim = self.input_featuremap_dim
+        self.bn_final= nn.BatchNorm2d(self.final_featuremap_dim)
+        self.relu_final = nn.ReLU(inplace=True)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(self.final_featuremap_dim , num_classes)
+
+
+
+        #for m in self.modules():
+            #if isinstance(m, nn.Conv2d):
+                #n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                #m.weight.data.normal_(0, math.sqrt(2. / n))
+            #elif isinstance(m, nn.BatchNorm2d):
+                #m.weight.data.fill_(1)
+                #m.bias.data.zero_()
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+                
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck1):
+                    nn.init.constant_(m.bn3.weight, 0)  # type: ignore[arg-type]
+                elif isinstance(m, BasicBlock1):
+                    nn.init.constant_(m.bn2.weight, 0)        
+
+    def pyramidal_make_layer(self, block, block_depth, stride=1):
+        downsample = None
+        if stride != 1: # or self.inplanes != int(round(featuremap_dim_1st)) * block.outchannel_ratio:
+            downsample = nn.AvgPool2d((2,2), stride = (2, 2), ceil_mode=True)
+
+        layers = []
+        self.featuremap_dim = self.featuremap_dim + self.addrate
+        layers.append(block(self.input_featuremap_dim, int(round(self.featuremap_dim)), stride, downsample))
+        for i in range(1, block_depth):
+            temp_featuremap_dim = self.featuremap_dim + self.addrate
+            layers.append(block(int(round(self.featuremap_dim)) * block.outchannel_ratio, int(round(temp_featuremap_dim)), 1))
+            self.featuremap_dim  = temp_featuremap_dim
+        self.input_featuremap_dim = int(round(self.featuremap_dim)) * block.outchannel_ratio
+
+        return nn.Sequential(*layers)
+
+    def _forward_impl(self, x: Tensor,use_feature=True) -> Tensor:
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        x = self.bn_final(x)
+        x = self.relu_final(x)
+        if use_feature:
+            return x
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+    def forward(self, x: Tensor,use_feature=True) -> Tensor:
+        return self._forward_impl(x,use_feature=use_feature)
+
+
+
+
+
+def pyramid(pretrained: bool = False, progress: bool = True, **kwargs: Any) -> ResNet:
+    return PyramidNet(BasicBlock1, **kwargs)
