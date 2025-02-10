a81b6e07df9437c2651bb954c68525b3a79b7155
Fateme <87362524+ftmsadeghi@users.noreply.github.com>
2025-02-10 16:33:16 +0330

    Update ResNet.py

--- model/ResNet.py
+++ model/ResNet.py
@@ -639,6 +639,48 @@ def wide_resnet101_2(pretrained: bool = False, progress: bool = True, **kwargs: 
     return _resnet('wide_resnet101_2', Bottleneck, [3, 4, 23, 3],
                    pretrained, progress, **kwargs)
 #------------------------------------------------------------------------------------------------
+class BasicBlock1(nn.Module):
+    outchannel_ratio = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock1, self).__init__()
+        self.bn1 = nn.BatchNorm2d(inplanes)
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn3 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+
+        out = self.bn1(x)
+        out = self.conv1(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            shortcut = self.downsample(x)
+            featuremap_size = shortcut.size()[2:4]
+        else:
+            shortcut = x
+            featuremap_size = out.size()[2:4]
+
+        batch_size = out.size()[0]
+        residual_channel = out.size()[1]
+        shortcut_channel = shortcut.size()[1]
+
+        if residual_channel != shortcut_channel:
+            padding = torch.autograd.Variable(torch.cuda.FloatTensor(batch_size, residual_channel - shortcut_channel, featuremap_size[0], featuremap_size[1]).fill_(0))
+            out += torch.cat((shortcut, padding), 1)
+        else:
+            out += shortcut
+
+        return out
+        
 class Bottleneck1(nn.Module):
     outchannel_ratio = 4
 
